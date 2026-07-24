731fecb5b7814162377fb60463bf3fe892cc293e
brayan_camilo <bryancaceres200517@gmail.com>
2026-07-23 19:41:32 -0500

    Eliminar seeders de prueba para despliegue en producción

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,25 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
-public function run(): void
-{
-    $this->call([
-        UserSeeder::class,
-        HabitacionSeeder::class,
-        HuespedSeeder::class,
-        ReservaSeeder::class,
-    ]);
-}
-
+    public function run(): void
+    {
+        $this->call([
+            UserSeeder::class,
+        ]);
+    }
 }
